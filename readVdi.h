5b64ba7d7973a0e3ccb31891118666178e6688fe
iasnijor <rojen.shahi3@gmail.com>
2019-04-03 12:51:12 -0400

    u

--- readVdi.h
+++ readVdi.h
@@ -12,50 +12,51 @@
 #include <unistd.h>
 #include <fcntl.h>
 using namespace std;
-int vdifileopen(VDIFile *v,  char* name){
 
+  int vdifileopen(VDIFile *v,  char* name){
     v->file = open(name,O_RDWR);
-
     return v->file;
-}
-void vdiFileClose(VDIFile *v){
-  if (!v)
-  return;
-   close(v->file);
+  }
+
+  void vdiFileClose(VDIFile *v){
+    if (!v)
+    return;
+    close(v->file);
   }
 
   int VDISeek (VDIFile* v, off_t off, int s){
     int loc= lseek(v->file,off,s);
     return loc;
   }
+
   ssize_t VDIread(VDIFile *v,void *buff,ssize_t num){
     ssize_t nBytes= read(v->file,buff,num);
     if (num != nBytes){
       cout <<"Error";
       return 1;
-          }
-          return nBytes;
-          v->cursor= v->header.offsetdata;
+    }
+    return nBytes;
+    v->cursor= v->header.offsetdata;
   }
-/*  int readMap(VDIFile *v, int headerMap[]){
+  /*  int readMap(VDIFile *v, int headerMap[]){
   off_t offset= lseek (v->file,v->header.offsetblock, SEEK_END);
-cout << dec<<v->header.offsetblock<< endl;
-    int map= read(v->file,headerMap,4*(v->header.blocks));
-cout << dec << "vdi" << map<< endl;
-    return 0;
+  cout << dec<<v->header.offsetblock<< endl;
+  int map= read(v->file,headerMap,4*(v->header.blocks));
+  cout << dec << "vdi" << map<< endl;
+  return 0;
 
   }*/
 
   int readMBR(VDIFile *f, BootRecord&  b){
-      off_t offset= lseek(f->file,f->header.offsetdata,SEEK_SET);
-      if (offset< 0) cout << "Error"<<endl;
-      int mbr = read(f->file,&b,sizeof(b));
-      return mbr;
+    off_t offset= lseek(f->file,f->header.offsetdata,SEEK_SET);
+    if (offset< 0) cout << "Error"<<endl;
+    int mbr = read(f->file,&b,sizeof(b));
+    return mbr;
   }
 
- int readSuperblock(VDIFile *f,int loc, Superblock& s){
-   off_t offset= lseek(f->file,f->header.offsetdata+loc,SEEK_SET);
-   if (offset<0) cout << "Error"<< endl;
+  int readSuperblock(VDIFile *f,int loc, Superblock& s){
+    off_t offset= lseek(f->file,f->header.offsetdata+loc,SEEK_SET);
+    if (offset<0) cout << "Error"<< endl;
     int super= read(f->file,&s, sizeof(s));
     if(super==1024)
     cout <<"no" << endl;
